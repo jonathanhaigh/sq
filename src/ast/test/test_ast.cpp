9d2c6e26a4bbda8ec4c046e1f1811dc61df6b2ab
Jonathan Haigh <jonathanhaigh@gmail.com>
2021-02-17 11:24:15 +0000

    Fix a bunch of ODR related CodeChecker issues
    CodeChecker was complaining about a bunch of ODR issues in the test code
    because the tests were defined in header files. Get rid of those
    complaints by defining the tests for each module in a cpp file and
    having a separate test executable per module.
    
    This is better anyway because it reduces coupling between the test
    modules.

--- src/ast/test/test_ast.cpp
+++ src/ast/test/test_ast.cpp
@@ -1,1 +1,324 @@
-#include "test/test_ast.h"
+#include "ast/ast.h"
+#include "ast/FilterSpec.h"
+#include "ast/ParseError.h"
+#include "test/FieldCallParams_test_util.h"
+#include "util/strutil.h"
+
+#include <gtest/gtest.h>
+#include <utility>
+
+namespace sq::test {
+
+using namespace sq::ast;
+using Size = Ast::Children::size_type;
+
+inline const auto no_filter_spec = FilterSpec{NoFilterSpec{}};
+
+inline void expect_node(
+    const Ast& node,
+    const char* const name,
+    const FieldCallParams& params,
+    const FilterSpec& filter_spec,
+    const Size noof_children
+)
+{
+    SCOPED_TRACE(testing::Message()
+        << "expect_node(node, "
+        << std::quoted(name) << ", "
+        << params << ", "
+        << util::variant_to_str(filter_spec) << ", "
+        << noof_children
+        << ")"
+    );
+    EXPECT_EQ(node.data().name(), name);
+    EXPECT_EQ(node.data().params(), params);
+    EXPECT_EQ(node.data().filter_spec(), filter_spec);
+    EXPECT_EQ(node.children().size(), noof_children);
+}
+
+inline void expect_plain_node(const Ast& node, const char*const name, const Size noof_children)
+{
+    SCOPED_TRACE("expect_plain_node");
+    expect_node(node, name, no_params, no_filter_spec, noof_children);
+}
+
+inline void expect_plain_leaf(const Ast& node, const char*const name)
+{
+    SCOPED_TRACE("expect_plain_leaf");
+    expect_plain_node(node, name, 0);
+}
+
+inline void expect_root(const Ast& node, const Size noof_children)
+{
+    SCOPED_TRACE("expect_root");
+    return expect_plain_node(node, "", noof_children);
+}
+
+inline void expect_equivalent_query(const char* const q1, const char* const q2)
+{
+    SCOPED_TRACE(testing::Message()
+        << "expect_equivalent_query("
+        << q1 << ", "
+        << q2 << ")"
+    );
+    ASSERT_EQ(generate_ast(q1), generate_ast(q2));
+}
+
+TEST(AstTest, DotExpression)
+{
+    const auto root = generate_ast("a.b");
+    expect_root(root, 1);
+
+    const auto& a = root.children().front();
+    expect_plain_node(a, "a", 1);
+
+    const auto& b = a.children().front();
+    expect_plain_leaf(b, "b");
+}
+
+TEST(AstTest, EquivalentQueries)
+{
+    expect_equivalent_query("a { b }", "a.b");
+    expect_equivalent_query("a { b { c } }", "a.b.c");
+    expect_equivalent_query("a { b { c { d } } }", "a.b.c.d");
+}
+
+TEST(AstTest, BraceExpression)
+{
+    const auto root = generate_ast("a { b c }");
+    expect_root(root, 1);
+
+    const auto& a = root.children().front();
+    expect_plain_node(a, "a", 2);
+
+    const auto& b = a.children().front();
+    expect_plain_leaf(b, "b");
+
+    const auto& c = a.children().back();
+    expect_plain_leaf(c, "c");
+}
+
+TEST(AstTest, MultipleEntrypoints)
+{
+    auto root = generate_ast("a b");
+    expect_root(root, 2);
+
+    const auto& a = root.children().front();
+    expect_plain_leaf(a, "a");
+
+    const auto& b = root.children().back();
+    expect_plain_leaf(b, "b");
+}
+
+using SimpleTestCase = std::tuple<const char*, FieldCallParams, FilterSpec>;
+class SimpleAstTest : public testing::TestWithParam<SimpleTestCase> { };
+
+TEST_P(SimpleAstTest, SimpleTest)
+{
+    const auto [query, params, filter_spec] = GetParam();
+
+    auto root = generate_ast(query);
+    expect_root(root, 1);
+
+    const auto& a = root.children().front();
+    expect_node(a, "a", params, filter_spec, 0);
+}
+
+
+
+INSTANTIATE_TEST_SUITE_P(
+    SimpleAstTestInstantiation,
+    SimpleAstTest,
+    testing::Values(
+        SimpleTestCase{
+            "a",
+            no_params,
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a[0]",
+            no_params,
+            ElementAccessSpec{0}
+        },
+        SimpleTestCase{
+            "a[99]",
+            no_params,
+            ElementAccessSpec{99}
+        },
+        SimpleTestCase{
+            "a[-1]",
+            no_params,
+            ElementAccessSpec{-1}
+        },
+        SimpleTestCase{
+            "a[-50]",
+            no_params,
+            ElementAccessSpec{-50}
+        },
+        SimpleTestCase{
+            "a[:]",
+            no_params,
+            SliceSpec{std::nullopt, std::nullopt, std::nullopt}
+        },
+        SimpleTestCase{
+            "a[1:]",
+            no_params,
+            SliceSpec{1, std::nullopt, std::nullopt}
+        },
+        SimpleTestCase{
+            "a[:1]",
+            no_params,
+            SliceSpec{std::nullopt, 1, std::nullopt}
+        },
+        SimpleTestCase{
+            "a[1:1]",
+            no_params,
+            SliceSpec{1, 1, std::nullopt}
+        },
+        SimpleTestCase{
+            "a[::]",
+            no_params,
+            SliceSpec{std::nullopt, std::nullopt, std::nullopt}
+        },
+        SimpleTestCase{
+            "a[1::]",
+            no_params,
+            SliceSpec{1, std::nullopt, std::nullopt}
+        },
+        SimpleTestCase{
+            "a[:1:]",
+            no_params,
+            SliceSpec{std::nullopt, 1, std::nullopt}
+        },
+        SimpleTestCase{
+            "a[1:1:]",
+            no_params,
+            SliceSpec{1, 1, std::nullopt}
+        },
+        SimpleTestCase{
+            "a[::1]",
+            no_params,
+            SliceSpec{std::nullopt, std::nullopt, 1}
+        },
+        SimpleTestCase{
+            "a[1::1]",
+            no_params,
+            SliceSpec{1, std::nullopt, 1}
+        },
+        SimpleTestCase{
+            "a[:1:1]",
+            no_params,
+            SliceSpec{std::nullopt, 1, 1}
+        },
+        SimpleTestCase{
+            "a[1:1:1]",
+            no_params,
+            SliceSpec{1, 1, 1}
+        },
+        SimpleTestCase{
+            "a(1)",
+            params(1),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(-1)",
+            params(-1),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(\"str\")",
+            params("str"),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(true)",
+            params(true),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(false)",
+            params(false),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(1, 2, 3, 4)",
+            params(1, 2, 3, 4),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(n=1)",
+            params(named("n", 1)),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(n=-1)",
+            params(named("n", -1)),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(n=\"str\")",
+            params(named("n", "str")),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(n=true)",
+            params(named("n", true)),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(n=false)",
+            params(named("n", false)),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(n=1, o=2, p=3, q=4)",
+            params(named("n", 1), named("o", 2), named("p", 3), named("q", 4)),
+            no_filter_spec
+        },
+        SimpleTestCase{
+            "a(1, true, \"str\", n=9)",
+            params(1, true, "str", named("n", 9)),
+            no_filter_spec
+        }
+    )
+);
+
+class InvalidQueryTest : public testing::TestWithParam<const char*> { };
+
+TEST_P(InvalidQueryTest, InvalidQuery)
+{
+    try {
+        const auto ast = generate_ast(GetParam());
+        FAIL() << "Expected ParseError; ast = " << ast;
+    }
+    catch(const ParseError& e)
+    {
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    InvalidQueryTestInstantiation,
+    InvalidQueryTest,
+    testing::Values(
+        ".a",
+        "a.",
+        "{",
+        "a {",
+        "}",
+        "a }",
+        "a { }",
+        "{ }",
+        "{ } a",
+        "{ } a",
+        "a(",
+        "a)",
+        "a(1",
+        "a(\"str)",
+        "a(p=)",
+        "a(p)",
+        "a(p=1,2)"
+        "a(p-x)"
+    )
+);
+
+} // namespace sq::test
--- src/common_types/test/test_common_types.cpp
+++ src/common_types/test/test_common_types.cpp
@@ -1,1 +1,194 @@
-#include "test/test_common_types.h"
+#include "common_types/FieldCallParams.h"
+#include "common_types/Primitive.h"
+#include "common_types/ArgumentTypeError.h"
+#include "common_types/ArgumentMissingError.h"
+
+#include <gtest/gtest.h>
+
+namespace sq::test {
+
+template <typename T>
+void test_primitive_type()
+{
+    SCOPED_TRACE(testing::Message()
+        << "test_primitive_type<"
+        << primitive_type_name_v<T>
+        << ">()"
+    );
+    EXPECT_NE(primitive_type_name_v<T>, nullptr);
+    EXPECT_EQ(primitive_type_name(T{}), primitive_type_name_v<T>);
+    EXPECT_EQ(primitive_type_name(Primitive{T{}}), primitive_type_name_v<T>);
+}
+
+TEST(CommonTypesTest, TestPrimitive)
+{
+    test_primitive_type<PrimitiveString>();
+    test_primitive_type<PrimitiveInt>();
+    test_primitive_type<PrimitiveFloat>();
+    test_primitive_type<PrimitiveBool>();
+}
+
+TEST(CommonTypesTest, TestPrimitiveToStr)
+{
+    EXPECT_EQ(
+        primitive_to_str(Primitive{PrimitiveString{"a string"}}),
+        "\"a string\""
+    );
+    EXPECT_EQ(
+        primitive_to_str(Primitive{PrimitiveString{"\\a string \\with backslashes\\"}}),
+        "\"\\\\a string \\\\with backslashes\\\\\""
+    );
+    EXPECT_EQ(
+        primitive_to_str(Primitive{PrimitiveInt{1234}}),
+        "1234"
+    );
+    EXPECT_EQ(
+        primitive_to_str(Primitive{PrimitiveInt{-1234}}),
+        "-1234"
+    );
+    EXPECT_EQ(
+        primitive_to_str(Primitive{PrimitiveFloat{1.1}}),
+        "1.1"
+    );
+    EXPECT_EQ(
+        primitive_to_str(Primitive{PrimitiveFloat{-1.1}}),
+        "-1.1"
+    );
+    EXPECT_EQ(
+        primitive_to_str(Primitive{PrimitiveBool{true}}),
+        "true"
+    );
+    EXPECT_EQ(
+        primitive_to_str(Primitive{PrimitiveBool{false}}),
+        "false"
+    );
+}
+
+class FieldCallParamsTest
+    : public testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        fcp_.pos_params().emplace_back(PrimitiveString{"str1"});
+        fcp_.pos_params().emplace_back(PrimitiveInt{1});
+        fcp_.named_params().emplace("s2", PrimitiveString{"str2"});
+        fcp_.named_params().emplace("i2", PrimitiveInt{2});
+    }
+
+    FieldCallParams fcp_;
+};
+
+TEST_F(FieldCallParamsTest, TestCorrectGet)
+{
+    SCOPED_TRACE(testing::Message() << "fcp_= " << fcp_);
+
+    EXPECT_EQ(fcp_.get<PrimitiveString>(0, "s1"), "str1");
+    EXPECT_EQ(fcp_.get<PrimitiveInt>(1, "i1"), 1);
+    EXPECT_EQ(fcp_.get<PrimitiveString>(2, "s2"), "str2");
+    EXPECT_EQ(fcp_.get<PrimitiveString>(3, "s2"), "str2");
+    EXPECT_EQ(fcp_.get<PrimitiveInt>(2, "i2"), 2);
+    EXPECT_EQ(fcp_.get<PrimitiveInt>(3, "i2"), 2);
+}
+
+TEST_F(FieldCallParamsTest, TestCorrectGetOptional)
+{
+    SCOPED_TRACE(testing::Message() << "fcp_= " << fcp_);
+
+    const auto* s1 = fcp_.get_optional<PrimitiveString>(0, "s1");
+    ASSERT_NE(s1, nullptr);
+    EXPECT_EQ(*s1, "str1");
+
+    const auto* i1 = fcp_.get_optional<PrimitiveInt>(1, "i1");
+    ASSERT_NE(i1, nullptr);
+    EXPECT_EQ(*i1, 1);
+
+    const auto* s2a = fcp_.get_optional<PrimitiveString>(2, "s2");
+    ASSERT_NE(s2a, nullptr);
+    EXPECT_EQ(*s2a, "str2");
+
+    const auto* s2b = fcp_.get_optional<PrimitiveString>(3, "s2");
+    ASSERT_NE(s2b, nullptr);
+    EXPECT_EQ(*s2b, "str2");
+
+    const auto* i2a = fcp_.get_optional<PrimitiveInt>(2, "i2");
+    ASSERT_NE(i2a, nullptr);
+    EXPECT_EQ(*i2a, 2);
+
+    const auto* i2b = fcp_.get_optional<PrimitiveInt>(3, "i2");
+    ASSERT_NE(i2b, nullptr);
+    EXPECT_EQ(*i2b, 2);
+}
+
+TEST_F(FieldCallParamsTest, TestGetWithInvalidType)
+{
+    SCOPED_TRACE(testing::Message() << "fcp_= " << fcp_);
+
+    EXPECT_THROW(fcp_.get<PrimitiveBool>(0, "s1"), ArgumentTypeError);
+    EXPECT_THROW(fcp_.get<PrimitiveBool>(1, "i1"), ArgumentTypeError);
+    EXPECT_THROW(fcp_.get<PrimitiveBool>(2, "s2"), ArgumentTypeError);
+    EXPECT_THROW(fcp_.get<PrimitiveBool>(3, "i2"), ArgumentTypeError);
+}
+
+TEST_F(FieldCallParamsTest, TestGetOptionalWithInvalidType)
+{
+    SCOPED_TRACE(testing::Message() << "fcp_= " << fcp_);
+
+    EXPECT_THROW(fcp_.get_optional<PrimitiveBool>(0, "s1"), ArgumentTypeError);
+    EXPECT_THROW(fcp_.get_optional<PrimitiveBool>(1, "i1"), ArgumentTypeError);
+    EXPECT_THROW(fcp_.get_optional<PrimitiveBool>(2, "s2"), ArgumentTypeError);
+    EXPECT_THROW(fcp_.get_optional<PrimitiveBool>(3, "i2"), ArgumentTypeError);
+}
+
+TEST_F(FieldCallParamsTest, TestGetWithMissingArgument)
+{
+    SCOPED_TRACE(testing::Message() << "fcp_= " << fcp_);
+
+    EXPECT_THROW(fcp_.get<PrimitiveBool>(4, "s3"), ArgumentMissingError);
+}
+
+TEST_F(FieldCallParamsTest, TestGetOptionalWithMissingArgument)
+{
+    SCOPED_TRACE(testing::Message() << "fcp_= " << fcp_);
+
+    EXPECT_EQ(fcp_.get_optional<PrimitiveBool>(4, "s3"), nullptr);
+}
+
+TEST_F(FieldCallParamsTest, TestEq)
+{
+    SCOPED_TRACE(testing::Message() << "fcp_= " << fcp_);
+
+    auto fcp_copy = fcp_;
+    SCOPED_TRACE(testing::Message() << "fcp_copy = " << fcp_copy);
+    EXPECT_TRUE(fcp_ == fcp_);
+    EXPECT_TRUE(fcp_copy == fcp_);
+    EXPECT_TRUE(fcp_ == fcp_copy);
+    EXPECT_FALSE(fcp_ != fcp_);
+    EXPECT_FALSE(fcp_copy != fcp_);
+    EXPECT_FALSE(fcp_ != fcp_copy);
+
+    auto fcp_different_pos = fcp_;
+    fcp_different_pos.pos_params()[1] = PrimitiveInt{2};
+    SCOPED_TRACE(testing::Message() << "fcp_different_pos = " << fcp_different_pos);
+
+    EXPECT_FALSE(fcp_different_pos == fcp_);
+    EXPECT_FALSE(fcp_ == fcp_different_pos);
+    EXPECT_TRUE(fcp_different_pos != fcp_);
+    EXPECT_TRUE(fcp_ != fcp_different_pos);
+
+    auto fcp_different_named = fcp_;
+    fcp_different_named.named_params().emplace("i3", PrimitiveInt{0});
+    SCOPED_TRACE(testing::Message() << "fcp_different_named = " << fcp_different_named);
+
+    EXPECT_FALSE(fcp_different_named == fcp_);
+    EXPECT_FALSE(fcp_ == fcp_different_named);
+    EXPECT_TRUE(fcp_different_named != fcp_);
+    EXPECT_TRUE(fcp_ != fcp_different_named);
+
+    EXPECT_FALSE(fcp_different_named == fcp_different_pos);
+    EXPECT_FALSE(fcp_different_pos == fcp_different_named);
+    EXPECT_TRUE(fcp_different_named != fcp_different_pos);
+    EXPECT_TRUE(fcp_different_pos != fcp_different_named);
+}
+
+} // namespace sq::test
