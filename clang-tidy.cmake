6b4924ca872ea335316a1c30b898e5355a06b9c1
Jonathan Haigh <jonathanhaigh@gmail.com>
2021-02-24 16:48:52 +0000

    Use sanitizers in Debug builds

---
+++ clang-tidy.cmake
@@ -0,0 +1,13 @@
+find_program(
+    SQ_CLANG_TIDY_PATH
+    NAMES clang-tidy clang-tidy-10 clang-tidy-11
+)
+if("${CMAKE_BUILD_TYPE}" STREQUAL Debug AND
+   NOT CMAKE_CROSSCOMPILING AND
+   SQ_CLANG_TIDY_PATH
+)
+    message("== Using Clang-Tidy: ${SQ_CLANG_TIDY_PATH}")
+    set(CMAKE_CXX_CLANG_TIDY ${SQ_CLANG_TIDY_PATH})
+else()
+    message("== Using Clang-Tidy: No")
+endif()
---
+++ optimization_flags.cmake
@@ -0,0 +1,24 @@
+set(SQ_OPTIMIZATION_FLAGS)
+
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL Clang OR
+    "${CMAKE_CXX_COMPILER_ID}" STREQUAL GNU
+)
+    if("${CMAKE_BUILD_TYPE}" STREQUAL Debug)
+        set(SQ_OPTIMIZATION_FLAGS
+            -Og
+            -fno-omit-frame-pointer
+            -fno-optimize-sibling-calls
+        )
+    else()
+        set(SQ_OPTIMIZATION_FLAGS
+            -O2
+        )
+    endif()
+endif()
+
+if (SQ_OPTIMIZATION_FLAGS)
+    message("== Using optimization flags: ${SQ_OPTIMIZATION_FLAGS}")
+    add_compile_options(${SQ_OPTIMIZATION_FLAGS})
+else()
+    message("== Using optimization flags: No")
+endif()
