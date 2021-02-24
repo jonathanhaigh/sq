5cc1321651ba7600565fdc6287241ca9bcb5846b
Jonathan Haigh <jonathanhaigh@gmail.com>
2021-02-24 16:48:52 +0000

    Use sanitizers in Debug builds

---
+++ sanitizer_flags.cmake
@@ -0,0 +1,36 @@
+set(SQ_SANITIZER_FLAGS)
+if("${CMAKE_BUILD_TYPE}" STREQUAL Debug)
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL Clang)
+        set(SQ_SANITIZER_FLAGS
+            -fsanitize=address
+            -fsanitize=undefined
+            -fsanitize=float-divide-by-zero
+            -fsanitize=local-bounds
+            -fsanitize=nullability
+            -fno-sanitize-recover=undefined
+            -fno-sanitize-recover=float-divide-by-zero
+            -fno-sanitize-recover=local-bounds
+            -fno-sanitize-recover=nullability
+        )
+    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL GNU)
+        set(SQ_SANITIZER_FLAGS
+            -fsanitize=address
+            -fsanitize=pointer-compare
+            -fsanitize=pointer-subtract
+            -fsanitize=leak
+            -fsanitize=undefined
+            -fsanitize=float-divide-by-zero
+            -fsanitize=float-cast-overflow
+            -fno-sanitize-recover=all
+            -fstack-protector-all
+        )
+    endif()
+endif()
+
+if (SQ_SANITIZER_FLAGS)
+    message("== Using sanitizer flags: ${SQ_SANITIZER_FLAGS}")
+    add_compile_options(${SQ_SANITIZER_FLAGS})
+    add_link_options(${SQ_SANITIZER_FLAGS})
+else()
+    message("== Using sanitizer flags: No")
+endif()
---
+++ warning_flags.cmake
@@ -0,0 +1,23 @@
+set(SQ_WARNING_FLAGS)
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL Clang OR
+    "${CMAKE_CXX_COMPILER_ID}" STREQUAL GNU 
+)
+    set(SQ_WARNING_FLAGS
+        -Wall
+        -Wextra
+        -pedantic-errors
+        -pedantic
+        -Wconversion
+        -Wsign-conversion
+        -Werror
+        -Wold-style-cast
+        -Woverloaded-virtual
+    )
+endif()
+
+if (SQ_WARNING_FLAGS)
+    message("== Using warning flags: ${SQ_WARNING_FLAGS}")
+    add_compile_options(${SQ_WARNING_FLAGS})
+else()
+    message("== Using warning flags: No")
+endif()
